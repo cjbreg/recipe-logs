f9daf3112fa9a4e20c9645ce5b49442c290a9c56
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-07-11 15:31:44 +0200

    feat: updated icons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,16 +15,42 @@ function MyApp({ Component, pageProps }: AppProps) {
 
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="mobile-web-app-capable" content="yes" />
 
+        <link
+          rel="apple-touch-icon"
+          sizes="192x192"
+          href="/icons/icon-192x192.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/icons/icon-152x152.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="shortcut icon" href="/favicon.ico" />
+
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#1BBD66" />
 
         <meta
           name="viewport"
           content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
         />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
+        <meta
+          name="description"
+          content="Simple app for collecting online recipies"
+        />
 
         <link rel="manifest" href="/manifest.json" />
       </Head>
