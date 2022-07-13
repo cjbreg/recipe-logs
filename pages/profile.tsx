d5979aa338d5913bb5ed4c08af576d4e4b306d4a
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-07-13 13:08:17 +0200

    feat: added wip indicator for profile

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Main from "../components/layout/Main";
+import Image from "next/image";
 
 const Profile = () => {
-  return <Main>profile</Main>;
+  return (
+    <Main>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <Image src="/images/undraw_building.svg" height={400} width={400} />
+        <p>Work in progress</p>
+      </div>
+    </Main>
+  );
 };
 
 export default Profile;
