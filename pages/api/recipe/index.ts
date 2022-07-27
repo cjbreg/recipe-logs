a4bc6d6a601743f462eb014c9d588d9cc38a4c36
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-07-27 17:38:30 +0200

    feat: basic REST page

---
+++ pages/api/recipe/index.ts
@@ -0,0 +1,35 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { createRecipe, getAllRecipes } from "../../../prisma/recipe";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    // const user: TokenData = await authenticateJWT(req, res).catch((message) => {
+    //   throw { message: message, noToken: true };
+    // });
+
+    switch (req.method) {
+      case "GET":
+        const recipes = await getAllRecipes();
+        return res.status(200).json(recipes);
+      case "POST":
+        const { newRecipe } = req.body;
+        console.log("recipeData", newRecipe);
+
+        const recipe = await createRecipe(newRecipe);
+        return res.status(200).json(recipe);
+      case "PUT":
+        return;
+      case "DELETE":
+        return;
+
+      default:
+        break;
+    }
+  } catch (error: any) {
+    if (error.noToken) return res.status(401).json({ message: error.message });
+    return res.status(500).json({ ...error, message: error.message });
+  }
+}
