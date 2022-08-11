3634a36f75029450a4bdbf65a15dcb1808f5faeb
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-08-11 12:14:53 +0200

    feat: init basic middleware verify

--- middleware.ts
+++ middleware.ts
@@ -2,15 +2,19 @@
 import { NextRequest, NextResponse } from 'next/server';
 import type { NextApiRequest } from 'next';
 import jwt from 'jsonwebtoken';
+import { verifyToken } from 'src/web/token';
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  console.log('middleware bb');
+  const authCookie: any = request.cookies.get('auth');
+  if (authCookie === undefined) {
+    return NextResponse.redirect(`${request.url}auth`);
+  }
 
   return NextResponse.next();
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: '/api/user'
+  matcher: '/'
 };
