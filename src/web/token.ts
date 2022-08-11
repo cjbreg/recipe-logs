4fa2103e94321411e15ad72aa1f9db65d3d60e5f
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-08-11 12:14:22 +0200

    feat: added token verify

---
+++ src/web/token.ts
@@ -0,0 +1,28 @@
+import { IncomingMessage } from 'http';
+import jwt from 'jsonwebtoken';
+import { NextApiRequestCookies } from 'next/dist/server/api-utils';
+import { getUserByEmail } from 'prisma/user';
+
+const SECRET_KEY = process.env.SECRET_KEY || '';
+export const cookieOptions = {
+  path: '/',
+  sameSite: true,
+  secure: process.env.NODE_ENV === 'production'
+};
+
+export const verifyToken = async (req: IncomingMessage & { cookies: NextApiRequestCookies }) => {
+  const { auth } = req.cookies;
+  if (!auth) return undefined;
+
+  const { token } = JSON.parse(auth);
+
+  try {
+    const userData = jwt.verify(token, SECRET_KEY);
+    if (!userData) return undefined;
+
+    const user = await getUserByEmail(userData.email);
+    return user;
+  } catch (error) {
+    return undefined;
+  }
+};
