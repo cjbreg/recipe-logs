9a2b8fe1eb849727a2e2bf028dc16528e7e13993
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-09-07 15:43:38 +0200

    feat: create favorite endpoint

---
+++ pages/api/recipe/favorite.ts
@@ -0,0 +1,33 @@
+import { TokenData } from '@Models/TokenData';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { authenticateJWT } from '..';
+import { toggleFavorite } from '../../../prisma/recipe';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const userData: TokenData = await authenticateJWT(req, res).catch((message) => {
+    throw { message: message, noToken: true };
+  });
+  console.log('here we go', req.body);
+
+  try {
+    const { recipeId, favorite } = req.body.params;
+    switch (req.method) {
+      case 'GET':
+        return;
+      case 'POST':
+        return;
+      case 'PUT':
+        console.log(recipeId, favorite);
+
+        const recipe = await toggleFavorite(recipeId, favorite);
+        return res.status(200).json(recipe);
+      case 'DELETE':
+        return;
+      default:
+        break;
+    }
+  } catch (error: any) {
+    if (error.noToken) return res.status(401).json({ message: error.message });
+    return res.status(500).json({ ...error, message: error.message });
+  }
+}
