9cb78ed9ae7b7fece98d851df181dbef21b182f3
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-09-07 14:38:50 +0200

    feat: added getRecipe by Id

--- pages/api/recipe/index.ts
+++ pages/api/recipe/index.ts
@@ -1,7 +1,7 @@
 import { TokenData } from '@Models/TokenData';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { authenticateJWT } from '..';
-import { createRecipe, deleteRecipe, getRecipes } from '../../../prisma/recipe';
+import { createRecipe, deleteRecipe, getRecipe, getRecipes } from '../../../prisma/recipe';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const userData: TokenData = await authenticateJWT(req, res).catch((message) => {
@@ -9,10 +9,18 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   });
 
   try {
+    const { recipeId } = req.body;
+    const { recipeId: queryId } = req.query;
+
     switch (req.method) {
       case 'GET':
-        const recipes = await getRecipes(userData.id);
-        return res.status(200).json(recipes);
+        if (!queryId) {
+          const recipes = await getRecipes(userData.id);
+          return res.status(200).json(recipes);
+        } else {
+          const recipe = await getRecipe(queryId.toString());
+          return res.status(200).json(recipe);
+        }
       case 'POST':
         const { newRecipe, metaData } = req.body;
         const recipe = await createRecipe(newRecipe, metaData, userData.id);
@@ -20,7 +28,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       case 'PUT':
         return;
       case 'DELETE':
-        const { recipeId } = req.body;
         const removedRecipe = await deleteRecipe(recipeId);
         return res.status(200).json(removedRecipe);
 
