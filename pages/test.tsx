fe70e98128fca71d16990eab668338b605f063b3
Breg,Coen C.J <c.breg@student.fontys.nl>
2022-09-07 12:35:44 +0200

    featL added test code for query

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,16 +1,48 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useFetchRecipes } from 'src/hooks/useRecipe';
-import { State } from 'src/store/reducers';
+import React from 'react';
+import { useFetchRecipes, useUploadRecipe } from 'src/hooks/useRecipe';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 const Test = () => {
-  const { accessToken } = useSelector((state: State) => state.authData);
+  const { data: recipes, isLoading, refetch, isFetching } = useFetchRecipes();
+  const recipeMutation = useUploadRecipe();
 
-  const { data: recipes, isLoading, refetch, isFetching } = useFetchRecipes(accessToken);
+  const handleTestMutation = () => {
+    recipeMutation.mutate({
+      newRecipe: {
+        name: 'Pastasalade met pesto, courgette en amandelen recept - Allerhande | Albert Heijn',
+        recipeUrl:
+          'https://www.ah.nl/allerhande/recept/R-R1196933/pastasalade-met-pesto-courgette-en-amandelen-advertorial',
+        durationMinutes: 69,
+        favorite: false,
+        backgroundImageUrl:
+          'https://static.ah.nl/static/recepten/img_RAM_PRD166576_1024x748_JPG.jpg',
+        userId: '62ebb9506953492bd08aa195',
+        categories: []
+      },
+      metaData: {
+        description:
+          'Zelf Pastasalade met pesto, courgette en amandelen maken? Met dit recept van Allerhande zet je een feestje op tafel. Bekijk ingrediënten en bereidingswijze!',
+        image: 'https://static.ah.nl/static/recepten/img_RAM_PRD166576_1024x748_JPG.jpg',
+        title: 'Pastasalade met pesto, courgette en amandelen recept - Allerhande | Albert Heijn',
+        author: 'Albert Heijn',
+        logo: 'https://static.ah.nl/ah-static/images/logo-allerhande.png',
+        publisher: 'Albert Heijn'
+      }
+    });
+  };
+
+  if (isLoading || isFetching) {
+    return (
+      <>
+        <div>Loading</div>
+      </>
+    );
+  }
 
   return (
     <>
       <div>{recipes ? recipes.length : 'error'}</div>
+      <button onClick={handleTestMutation}>click</button>
     </>
   );
 };
